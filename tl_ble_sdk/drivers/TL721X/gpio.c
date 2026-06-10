064c7f6f4331d18965ea26fe8884faa8919f76e9
Zhihan Tao <zhihan.tao@telink-semi.com>
2026-06-10 16:40:33 +0800

    feat: sync with src.
    - feat(zephyr): optimize the current.
    - 5bd8c9193f735241cff24f0aacb83152ab959f11

--- tl_ble_sdk/drivers/TL721X/gpio.c
+++ tl_ble_sdk/drivers/TL721X/gpio.c
@@ -166,6 +166,9 @@ void gpio_ds_dis(gpio_pin_e pin)
  * @param[in]  pin  - select the specified GPIO.
  * @return     none.
  */
+#ifdef BLC_ZEPHYR_BLE_INTEGRATION
+    _attribute_ram_code_sec_noinline_ /* ble used */
+#endif
 void gpio_shutdown(gpio_pin_e pin)
 {
     unsigned short group = pin & 0xf00;
